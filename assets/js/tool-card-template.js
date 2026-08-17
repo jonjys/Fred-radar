9fc651cb71c101bae06cf88de2ba8b24ea52cb81
FRED <fkornelind@hotmail.com>
2026-08-17 15:12:42 +0200

    v0.5: tool-card with Alternativ links

--- assets/js/tool-card-template.js
+++ assets/js/tool-card-template.js
@@ -65,6 +65,7 @@
         <div class="tool-actions">
           <a class="btn btn-primary" href="/go/?tool=${tool.id}&src=${src}" target="_blank" rel="sponsored noopener">Besök ${tool.name}</a>
           <a class="btn btn-ghost" href="/go/?tool=${tool.id}&src=${src}-website" target="_blank" rel="sponsored noopener">Webbplats</a>
+          <a class="btn btn-ghost" href="/alternativ/${tool.id}.html">Alternativ</a>
         </div>
         <p class="ad-note">Annonslänk – vi kan få provision. Påverkar inte priset eller rankingen.</p>
       </article>
